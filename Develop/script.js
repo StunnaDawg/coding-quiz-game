a4963cdf182ecfa1a40faed87bd34e83dd813554
StunnaDawg <jonsonallen9@gmail.com>
2023-02-05 15:43:38 -0400

    start button function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 //start button
 const button = document.querySelector('#start')
+const questionOne = document.querySelector('.question-one')
+const mainSection = document.querySelector('.main-section')
 
 button.addEventListener('click', function() {
+    questionOne.style.display = "block";
+    mainSection.style.display = "none"
 })
